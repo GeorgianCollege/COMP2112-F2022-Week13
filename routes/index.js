7673ea74b26bc7cc7425c8ea71a47043b649f5a6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-12-05 10:22:07 -0500

    modified the index router

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/home', (req, res, next) => {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/about', (req, res, next) => {
+    res.render('index', { title: 'About Us', page: 'about' });
+});
+router.get('/projects', (req, res, next) => {
+    res.render('index', { title: 'Our Projects', page: 'projects' });
+});
+router.get('/services', (req, res, next) => {
+    res.render('index', { title: 'Our Services', page: 'services' });
+});
+router.get('/contact', (req, res, next) => {
+    res.render('index', { title: 'Contact Us', page: 'contact' });
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
