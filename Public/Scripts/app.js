de158e6fd9171f710818fe5439fee68dbc412a8f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-12-05 10:48:52 -0500

    enabled button functionality on home page

--- Public/Scripts/app.js
+++ Public/Scripts/app.js
@@ -2,6 +2,9 @@
 (function () {
     function start() {
         console.log("App Started...");
+        $("#aboutUsButton").on("click", () => {
+            location.href = "/about";
+        });
     }
     window.addEventListener("load", start);
 })();
--- Public/Scripts/app.ts
+++ Public/Scripts/app.ts
@@ -4,6 +4,10 @@
     function start()
     {
         console.log("App Started...");
+
+        $("#aboutUsButton").on("click", ()=>{
+            location.href = "/about";
+        });
     }
 
     window.addEventListener("load", start);
